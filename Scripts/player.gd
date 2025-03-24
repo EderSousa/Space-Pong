1c2335dfebce13a7bac8fc5b8a4e00b9a6f320f6
Eder Sousa <Ederbruno2017@gmail.com>
2025-03-24 18:51:45 -0300

    Ula ula

---
+++ Scripts/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var speed = 100
+var ball
+
+func _ready():
+	ball = get_parent().get_node("Ball")
+
+# É executado todo tempo, a cada quadro físico do jogo
+func _physics_process(delta):
+	velocity.x = 0
+	if Input.is_action_pressed("ui_left") and ball.started == true:
+		velocity.x -= speed
+	if Input.is_action_pressed("ui_right") and ball.started == true:
+		velocity.x += speed
+		
+	move_and_collide(velocity * delta)
